735033efafe2a19a268b3eb05bc8d3110b8826ef
Sandesh SIngh <sandeshsingh201106@gmail.com>
2025-12-30 12:08:37 +0530

    Added Search Button

--- movie-desc/src/App.jsx
+++ movie-desc/src/App.jsx
@@ -1,6 +1,9 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Search from './components/Search'
 
 const App = () => {
+
+  const [searchTerm,setSearchTerm]=useState("");
   return (
    <main>
      <div className="pattern"/>
@@ -9,11 +12,11 @@ const App = () => {
         <header>
           <img src="./hero.png" alt="Hero Banner" />
           <h1>Find <span className="text-gradient">Movies</span> You'll Enjoy Without the Hassle</h1>
-       
         </header>
+        {/* state can also be passed as a props? */}
+        <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
         </div>
    </main>
   )
 }
-
 export default App
