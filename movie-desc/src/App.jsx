c71a4babc3973a39eb7d38a81f3b404b495e8884
Sandesh SIngh <sandeshsingh201106@gmail.com>
2025-12-30 13:18:49 +0530

    Added  movie api

--- movie-desc/src/App.jsx
+++ movie-desc/src/App.jsx
@@ -1,9 +1,34 @@
 import React, { useState } from 'react'
 import Search from './components/Search'
-
+import { useEffect } from 'react';
+const API_BASE_URL= 'https://api.themoviedb.org/3';
+const API_KEY=import.meta.env.VITE_TMDB_API_KEY;
+const API_OPTIONS={
+  method:'GET',
+  headers:{
+    accept:'application/json',
+    Authorization:`Bearer ${API_KEY}`
+  }
+}
 const App = () => {
+   const fetchMovies=async()=>{
+    
+   try{
+    const endpoint=`${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
+    const response=await fetch(endpoint,API_OPTIONS);
+    if(!response.ok){
+      throw new Error('Failed to fetch movies');
+    }
+   }catch{
 
+   }
+
+   }
   const [searchTerm,setSearchTerm]=useState("");
+
+  useEffect(()=>{
+    
+  },[]);
   return (
    <main>
      <div className="pattern"/>
